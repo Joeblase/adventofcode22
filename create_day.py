2c5e723dfb37287c293d7f0a1dba84bb4211b59a
Joe <joeblaset@gmail.com>
2023-01-02 15:34:10 -0500

    update create_day.py

--- create_day.py
+++ create_day.py
@@ -1,19 +1,39 @@
 import datetime
 import os
 
+months = {
+    1: 'Jan',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Apr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Aug',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec'
+}
+
+
+def now():
+    return datetime.datetime.now()
+
+
 while True:
     year_input = input('Enter year (or press enter for current date): ')
     if str(year_input) == '':
-        year = str(datetime.datetime.now().year)
+        year = str(now().year)
         break
-    if int(year_input) in range(2015, datetime.datetime.now().year + 1):
+    if int(year_input) in range(2015, now().year + 1):
         year = str(year_input)
         break
 
 while True:
     day_input = input('Enter day (or press enter for current date): ')
     if day_input == '':
-        day = datetime.datetime.now().day
+        day = now().day
         break
     if int(day_input) in range(1, 26):
         day = int(day_input)
@@ -38,6 +58,7 @@ if not os.path.exists(f'{path}/input.txt'):
 if not os.path.exists(f'{path}/{day_name}.py'):
     with open(f'{path}/{day_name}.py', 'w') as f:
         f.write(
+            f"# Created on {months[now().month]} {now().day} {now().year}\n"
             f"# https://adventofcode.com/{year}/day/{day}\n"
             "\n"
             "with open('input.txt') as input_file:\n"
